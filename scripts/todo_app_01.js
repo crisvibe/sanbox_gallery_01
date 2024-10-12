7d9e844bce04ff3058add378b0f6478ddbf9d7b5
crisvibe <devcris1@outlook.com>
2024-10-12 15:18:13 +0200

    Todo app working

--- scripts/todo_app_01.js
+++ scripts/todo_app_01.js
@@ -10,6 +10,34 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        inputBox.value =  "";
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName = "SPAN") {
+        e.target.parentElement.remove();
+    }
+    saveData();
+}, 
+false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function displayData() {
+    if (localStorage.data != "") {
+        // listContainer.innerHTML = localStorage.data;
+        listContainer.innerHTML = localStorage.getItem("data");
     }
 }
+displayData();
